4c9f6f81529996641b2c948f4eab891eb12845fd
Nathalia Dias <nathaliapiresdias0@gmail.com>
2024-08-05 10:49:28 -0300

    Concluído
    Signed-off-by: Nathalia Dias <nathaliapiresdias0@gmail.com>

---
+++ bee1003/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace bee1003{
+    class Program{
+        static void Main(string[] args){
+            int A = int.Parse(Console.ReadLine());
+            int B = int.Parse(Console.ReadLine());
+            int SOMA = A + B;
+            System.Console.WriteLine($"SOMA = {SOMA}");
+        }
+    }
+}
